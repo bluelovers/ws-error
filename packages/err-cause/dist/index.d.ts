21feba2940aae29023fac2ab5836cac1e2a21940
bluelovers <codelovers@users.sourceforge.net>
2022-08-31 04:55:45 +0800

    deps: update deps

--- packages/err-cause/dist/index.d.ts
+++ packages/err-cause/dist/index.d.ts
@@ -1,11 +1,14 @@
 export declare function _isError<E extends Error>(error: unknown): error is E;
-export declare type IErrorWithCause<E extends Error = Error, C extends Error = Error> = E & {
+export type IErrorWithCause<E extends Error = Error, C extends Error = Error> = E & {
 	cause: C;
 };
 export declare function isSupportedErrorCause<EC = ErrorConstructor>(sc?: EC): boolean;
 export declare function errCause<E extends Error, C extends Error>(error: E, options: C | {
 	cause: C;
 }): IErrorWithCause<E, C>;
-export default errCause;
+
+export {
+	errCause as default,
+};
 
 export {};
